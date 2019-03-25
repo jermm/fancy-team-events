e5d7843633e26aa499ebad918d6b1c4719125986
Vijay Enthran Varadarajan Subramanian <vijay.enthran.varadarajan.subramanian@sap.com>
2019-03-25 15:04:47 -0700

    initial Commit

--- fancy-team-client/src/Routes.js
+++ fancy-team-client/src/Routes.js
@@ -14,6 +14,7 @@ function Routes(){
           <Switch>
               <Route exact path='/' component={ App } />
               <Route exact path='/login' component={ Login } />
+              <Route exact path='/events/create' component={ Login } />
               <Route component={ Handle404 } />
           </Switch>
           </Router>
