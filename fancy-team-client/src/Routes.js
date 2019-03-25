178f3cd49bf5759b3b9830922a5372998021f02d
Raymond Chan <raymond.chan@sap.com>
2019-03-25 15:18:55 -0700

    Add routes, create event button, and event list

--- fancy-team-client/src/Routes.js
+++ fancy-team-client/src/Routes.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import App from './App';
 import Login from './components/Login/view';
+import Event from './components/Event/event';
 import Handle404 from './components/404/view';
 
 import store from './Redux/store';
@@ -14,6 +15,7 @@ function Routes(){
           <Switch>
               <Route exact path='/' component={ App } />
               <Route exact path='/login' component={ Login } />
+              <Route exact path='/event' component={ Event } />
               <Route component={ Handle404 } />
           </Switch>
           </Router>
--- fancy-team-client/src/components/Event/Event.js
+++ fancy-team-client/src/components/Event/Event.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-import {EventList} from "./EventList";
+import { EventList } from "./EventList";
+import { Link } from 'react-router-dom';
 
 class Event extends Component {
     render () {
         return (
             <div className="Event">
                 <EventList />
+                <Link to="/event/create">
+                    <img src="https://img.icons8.com/color/48/000000/plus.png"></img>
+                </Link>
             </div>
         )
     }
 }
+
+export default Event;
