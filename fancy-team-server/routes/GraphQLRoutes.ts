18b42be4bd28fa4a3c181a669cbf54f0d7cd4d78
Sam Thambu <sam.thambu@sap.com>
2019-03-25 13:58:06 -0700

    wrong editing

--- fancy-team-server/routes/GraphQLRoutes.ts
+++ fancy-team-server/routes/GraphQLRoutes.ts
@@ -1,19 +1,16 @@
 import * as express from "express";
-// import graphqlHTTP from 'express-graphql';
-// import schemas from  '../types/index';
+import graphqlHTTP from 'express-graphql';
+import schemas from  '../types/index';
 
 export class GraphQLRoutes {
     static map(app: express.Application): void {
-        app.use('/graphql', (req, res) => {
-            res.send('hello world');
-        });
-        // // Add GraphQL to express route
-        // app.use('/graphql',
-        //     // Creates a GraphQLHTTP per request
-        //     graphqlHTTP({
-        //         schema: schemas,
-        //         graphiql: true
-        //     }));
+        // Add GraphQL to express route
+        app.use('/graphql',
+            // Creates a GraphQLHTTP per request
+            graphqlHTTP({
+                schema: schemas,
+                graphiql: true
+            }));
     }
 }
 
