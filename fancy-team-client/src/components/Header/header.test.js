34817664165bdc00275535cea362e55bba9dda1e
Raymond Chan <raymond.chan@sap.com>
2019-03-28 11:53:05 -0700

    basic enzyme, snapshot test

--- fancy-team-client/src/components/Header/header.test.js
+++ fancy-team-client/src/components/Header/header.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './header';
 
+import { shallow } from 'enzyme';
+
 it('Header renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Header />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('Using shadown - should render correctly with no props', () => {
+    const component = shallow(<Header />);
+    expect(component).toMatchSnapshot();
+  });
