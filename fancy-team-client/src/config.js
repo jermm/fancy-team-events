4f5ed28cb7a11f4288116a96c4c98f29cfaaa1d1
Jeremy Muonio <jjeerr@gmail.com>
2019-04-01 19:14:29 +0000

    changed redirect to aws ip

--- fancy-team-client/src/config.js
+++ fancy-team-client/src/config.js
@@ -2,11 +2,11 @@ const Config = {
     oidc: {
       clientId: '0oadrmminV9IDToNc356',
       issuer: 'https://dev-751219.okta.com/oauth2/default',
-      redirectUri: 'http://localhost:3000/implicit/callback',
+      redirectUri: 'http://34.211.115.42/implicit/callback',
       scope: 'profile email',
     },
     resourceServer: {
-      eventsUrl: 'http://localhost:4000/graphql',
+      eventsUrl: 'http://34.211.115.42/graphql',
     }
 };
 
