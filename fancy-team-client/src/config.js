ae36fa0116c3b9ec2504b17c56aa0b89dc20b878
Vijay Enthran Varadarajan Subramanian <vijay.enthran.varadarajan.subramanian@sap.com>
2019-04-02 10:47:34 -0700

    Support env variables

--- fancy-team-client/src/config.js
+++ fancy-team-client/src/config.js
@@ -1,13 +1,25 @@
+console.log(process.env);
+
+const {
+  REACT_APP_clientid,
+  REACT_APP_issuer,
+  REACT_APP_redirectUri,
+  REACT_APP_scope,
+  REACT_APP_eventsUrl,
+  REACT_APP_googleAutoCompleteURL
+} = process.env;
+
 const Config = {
     oidc: {
-      clientId: '0oaeua30mnzGCYZcN356',
-      issuer: 'https://dev-751219.okta.com/oauth2/default',
-      redirectUri: 'http://localhost:3000/implicit/callback',
-      scope: 'profile email',
+      clientId: REACT_APP_clientid,
+      issuer: REACT_APP_issuer,
+      redirectUri: REACT_APP_redirectUri,
+      scope: REACT_APP_scope,
     },
     resourceServer: {
-      eventsUrl: 'http://localhost:4000/graphql',
-    }
+      eventsUrl: REACT_APP_eventsUrl,
+    },
+    googleAutoCompleteURL : REACT_APP_googleAutoCompleteURL
 };
 
 export default Config;
