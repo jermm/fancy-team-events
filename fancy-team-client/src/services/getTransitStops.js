34662901fd56c92383b62b1aa3fc21fabf6ca893
Jeremy Muonio <jeremy.muonio@sap.com>
2019-04-05 14:01:02 -0700

    ok transit stuff seems ok

--- fancy-team-client/src/services/getTransitStops.js
+++ fancy-team-client/src/services/getTransitStops.js
@@ -17,22 +17,22 @@ export const getStopsFromId = (id) => {
 };
 
 export const getStopsFromGeo = (lat, long) => {
-    const requestAddress = `https://transit.api.here.com/v3/stations/by_geocoord.json?app_id=${appId}&app_code=${appCode}&radius=1000&center=${lat},${long}`;
+    const requestAddress = `https://transit.api.here.com/v3/stations/by_geocoord.json?app_id=${appId}&app_code=${appCode}&radius=500&max=20&center=${lat},${long}`;
     return fetch(requestAddress).then(response => {
         return response.json();
     }).then(res => {
         let transportNames = [];
         let result = res.Res;
         if (result && result.Stations && result.Stations.Stn && result.Stations.Stn.length > 0) {
-            console.log(result.Stations.Stn[0]);
-
-            result.Stations.Stn[0].Transports["Transport"].forEach(function (transport) {
-                const transportMatch = (name) => {
-                    return name === transport.name;
-                };
-                if (transportNames.findIndex(transportMatch) === -1)  {
-                    transportNames.push(transport.name);
-                }
+            result.Stations.Stn.forEach(function (stations) {
+                stations.Transports["Transport"].forEach(function (transport) {
+                    const transportMatch = (name) => {
+                        return name === transport.name;
+                    };
+                    if (transportNames.findIndex(transportMatch) === -1)  {
+                        transportNames.push(transport.name);
+                    }
+                });
             });
         }
         return transportNames;
